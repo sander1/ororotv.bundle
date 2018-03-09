450564d176fba62d88f06beee31ab8396c985b82
Sander <sander@plex.tv>
2018-03-09 20:23:47 +0100

    Partial rewrite after site changes

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -6,33 +6,31 @@ BASE_URL = 'https://ororo.tv'
 CHANNELS_URL = '%s/en/channels' % (BASE_URL)
 YT_URL = 'https://www.youtube.com/watch?v=%s'
 
+RE_CHANNELS_JSON = Regex('"items":(\[.+?\])\};')
+
 ####################################################################################################
 def Start():
 
 	ObjectContainer.title1 = NAME
 	HTTP.CacheTime = CACHE_1HOUR
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
 
 ####################################################################################################
 @handler('/video/ororotv', NAME, art=ART, thumb=ICON)
 def MainMenu():
 
 	oc = ObjectContainer()
-	html = HTML.ElementFromURL(CHANNELS_URL)
+	page = HTTP.Request(CHANNELS_URL).content
+	json = RE_CHANNELS_JSON.search(page).group(1)
+	json_obj = JSON.ObjectFromString(json)
 
-	channels = html.xpath('//div[@class="channel"]')
-
-	for channel in channels:
-
-		url = channel.xpath('.//a/@href')[0]
-		title = channel.xpath('.//span[@class="title"]/text()')[0]
-		summary = channel.xpath('.//p[@class="desc"]/text()')[0]
-		thumb = channel.xpath('.//img/@src')[0]
+	for channel in json_obj:
 
 		oc.add(DirectoryObject(
-			key = Callback(Channel, url=url, title=title),
-			title = title,
-			summary = summary,
-			thumb = thumb
+			key = Callback(Channel, url=channel['url'], title=channel['title']),
+			title = channel['title'],
+			summary = channel['description'],
+			thumb = channel['image'].replace('/thumb_', '/')
 		))
 
 	return oc
@@ -44,13 +42,13 @@ def Channel(url, title):
 	oc = ObjectContainer(title2=title)
 	html = HTML.ElementFromURL('%s%s' % (BASE_URL, url))
 
-	videos = html.xpath('//div[contains(@class, "js-episode-wrapper")]')[:50]
+	videos = html.xpath('//a[@class="js-episode"]')
 
 	for video in videos:
 
-		yt_id = video.xpath('.//a/@href')[0].strip('#')
-		title = video.xpath('.//div[@class="name"]/text()')[0]
-		thumb = video.xpath('.//img/@data-original')[0]
+		yt_id = video.xpath('./@href')[0].strip('#')
+		title = video.xpath('./img/@alt')[0]
+		thumb = video.xpath('./img/@data-original')[0]
 
 		oc.add(VideoClipObject(
 			url = YT_URL % (yt_id),
